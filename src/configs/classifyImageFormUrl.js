948a25e2607c045da13318311a862d7aa9d8fcf9
Quang Huy <quanghuy2003.hh@gmail.com>
2025-06-09 22:25:00 +0700

    fix: modal

--- src/configs/classifyImageFormUrl.js
+++ src/configs/classifyImageFormUrl.js
@@ -1,10 +1,7 @@
-// Định nghĩa hàm classifyImageFromUrl
 const classifyImageFromUrl = async (imageUrl) => {
   try {
-    // Tải mô hình CLIP
-    const { pipeline } = await import('@xenova/transformers');
-    const model = await pipeline('image-classification');
-    // Phân loại hình ảnh
+    const module = await import('@xenova/transformers');
+    const model = await module.pipeline('image-classification');
     const result = await model(imageUrl);
     if (!result || result.length === 0) {
       throw new Error('No classification results found for the image.');
