00327c780321b45d13c893ab579c28e2a5b699b1
Quang Huy <quanghuy2003.hh@gmail.com>
2025-06-09 22:40:43 +0700

    fix: modal v2

--- src/configs/classifyImage .js
+++ src/configs/classifyImage .js
@@ -2,6 +2,7 @@ const classifyImage = async (imageUrl) => {
   const { classifyImageFromUrl } = await import(
     './configs/classifyImageFromUrl.mjs'
   );
+
   return classifyImageFromUrl(imageUrl);
 };
 
