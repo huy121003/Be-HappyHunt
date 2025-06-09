c3662cf97d9a7dad8e2eaf996ce2f0016edecda5
Quang Huy <quanghuy2003.hh@gmail.com>
2025-06-09 22:27:35 +0700

    fix: modal

--- src/configs/classifyImageFormUrl.js
+++ src/configs/classifyImageFormUrl.js
@@ -1,7 +1,7 @@
 const classifyImageFromUrl = async (imageUrl) => {
   try {
-    const module = await import('@xenova/transformers');
-    const model = await module.pipeline('image-classification');
+    const { pipeline } = await import('@xenova/transformers').then((m) => m);
+    const model = await pipeline('image-classification');
     const result = await model(imageUrl);
     if (!result || result.length === 0) {
       throw new Error('No classification results found for the image.');
