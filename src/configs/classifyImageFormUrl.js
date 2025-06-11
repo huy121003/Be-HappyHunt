a94f4c1991e30870588c661166a4a4a82eab7d01
Quang Huy <quanghuy2003.hh@gmail.com>
2025-06-11 09:46:43 +0700

    fix: check Image v4

--- src/configs/classifyImageFormUrl.js
+++ src/configs/classifyImageFormUrl.js
@@ -1,19 +1,19 @@
-// Định nghĩa hàm classifyImageFromUrl
-const classifyImageFromUrl = async (imageUrl) => {
-  try {
-    // Tải mô hình CLIP
-    const { pipeline } = await import('@xenova/transformers');
-    const model = await pipeline('image-classification');
-    // Phân loại hình ảnh
-    const result = await model(imageUrl);
-    if (!result || result?.length === 0) {
-      throw new Error('No classification results found for the image.');
-    }
-    console.log('Classification result:', result);
-    return result;
-  } catch (error) {
-    throw error;
-  }
-};
+// // Định nghĩa hàm classifyImageFromUrl
+// const classifyImageFromUrl = async (imageUrl) => {
+//   try {
+//     // Tải mô hình CLIP
+//     const { pipeline } = await import('@xenova/transformers');
+//     const model = await pipeline('image-classification');
+//     // Phân loại hình ảnh
+//     const result = await model(imageUrl);
+//     if (!result || result?.length === 0) {
+//       throw new Error('No classification results found for the image.');
+//     }
+//     console.log('Classification result:', result);
+//     return result;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
 
-module.exports = { classifyImageFromUrl };
+// module.exports = { classifyImageFromUrl };
